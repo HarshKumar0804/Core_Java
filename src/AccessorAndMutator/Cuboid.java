e951d138352b3aefdc279d29ce2769ab57cde16e
Harsh Kumar <hk82357@gmail.com>
2024-08-20 22:11:23 +0530

    Customize accessor and mutator

--- src/AccessorAndMutator/Cuboid.java
+++ src/AccessorAndMutator/Cuboid.java
@@ -6,39 +6,81 @@ public class Cuboid {
     private double breadth;
     private  double height;
 
-    // Accessor and Getter Methods
-    public double getLength() {
-        return length;
+//    // Accessor and Getter Methods
+//    public double getLength() {
+//        return length;
+//    }
+//
+//    public double getBreadth() {
+//        return breadth;
+//    }
+//
+//    public double getHeight() {
+//        return height;
+//    }
+//
+//    // Mutator and Setter Methods
+//    public void setLength(double length) {
+//        this.length = length;
+//    }
+//
+//    public void setBreadth(double breadth) {
+//        this.breadth = breadth;
+//    }
+//
+//    public void setHeight(double height) {
+//        this.height = height;
+//    }
+//
+//    // Method calling
+//    public double volume() {
+//        return length * breadth * height;
+//    }
+//
+//    public String toString(){
+//        String s = "".format("Length: %.2f\nBreadth: %.2f\nHeight: %.2f\nVolume: %.2f", this.getLength(), this.getBreadth(), this.getHeight(), this.volume());
+//        return s;
+//    }
+
+    // Customize accessor and mutator
+    public String getLength() {
+        return length + " meters";
+    }
+    public String getBreadth() {
+        return breadth + " meters";
+    }
+    public String getHeight() {
+        return height + " meters";
     }
 
-    public double getBreadth() {
-        return breadth;
+    public boolean setLength(double length) {
+        if(length >= 1.0) {
+            this.length = length;
+            return true;
+        }
+        return false;
     }
-
-    public double getHeight() {
-        return height;
+    public boolean setBreadth(double breadth) {
+        if(breadth >= 1.0) {
+            this.breadth = breadth;
+            return true;
+        }
+        return false;
     }
-
-    // Mutator and Setter Methods
-    public void setLength(double length) {
-        this.length = length;
+    public boolean setHeight(double height) {
+        if(height >= 1.0) {
+            this.height = height;
+            return true;
+        }
+        return false;
     }
-
-    public void setBreadth(double breadth) {
-        this.breadth = breadth;
-    }
-
-    public void setHeight(double height) {
-        this.height = height;
-    }
-
-    // Method calling
     public double volume() {
         return length * breadth * height;
     }
 
-    public String toString(){
-        String s = "".format("Length: %.2f\nBreadth: %.2f\nHeight: %.2f\nVolume: %.2f", this.getLength(), this.getBreadth(), this.getHeight(), this.volume());
-        return s;
-    }
+
+
+//    Q.) Add toString methods in cuboid class to print the details
+//    Q.) Take input from console to set values in cuboid objects, create at least three object
+
 }
--- src/AccessorAndMutator/Demo.java
+++ src/AccessorAndMutator/Demo.java
@@ -8,14 +8,30 @@ public class Demo {
         c1.setLength(12.6);
         c1.setBreadth(4.4);
         c1.setHeight(3.3);
-        System.out.println(c1.getLength());
-        System.out.println(c1.getBreadth());
-        System.out.println(c1.getHeight());
+        System.out.println("Length: " + c1.getLength());
+        System.out.println("Breadth: " + c1.getBreadth());
+        System.out.println("Height: " + c1.getHeight());
 
-        System.out.println("Volume is " + c1.volume());
+        System.out.println("Volume is " + c1.volume() + " sq meters");
+//
+//        // Q.) Implementing toString method
+//        System.out.println(c1);
 
-        // Q.) Implementing toString method
-        System.out.println(c1);
+        Cuboid c2 = new Cuboid();
+        if (c2.setLength(6.6) != true) {
+            System.out.println("Invalid Length");
+        }
+        if (c2.setBreadth(5.5) != true) {
+            System.out.println("Invalid Breadth");
+        }
+        if (c2.setHeight(2.2) != true) {
+            System.out.println("Invalid Height");
+        }
+        System.out.println("Length: " + c2.getLength());
+        System.out.println("Breadth: " + c2.getBreadth());
+        System.out.println("Height: " + c2.getHeight());
+
+        System.out.println("Volume is " + c2.volume() + " sq meters");
 
     }
 }
